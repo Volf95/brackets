2f04bf443a74ec3f6b65c174c83e5f0546460879
Laziz Khudayberdiev <57383456+Volf95@users.noreply.github.com>
2020-03-22 23:30:10 +0500

    feat: init task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  if (bracketsConfig.length % 2 !== 0 || str.length % 2 !== 0) {
+    return false;
+  }
+
+  if (Array.isArray(bracketsConfig)) {
+
+  }
+
+  if (arr.length !== 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
